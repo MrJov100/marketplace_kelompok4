923c3c18ad5cf489236eaea55fb1fc8e8d5e2031
Jovandi <162522277+MrJov100@users.noreply.github.com>
2024-09-11 12:48:44 +0700

    icon app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:marketplace_kelompok4/pages/bottomnav.dart';
-import 'package:marketplace_kelompok4/pages/home.dart';
-import 'package:marketplace_kelompok4/pages/onboarding.dart';
-import 'package:marketplace_kelompok4/pages/product_detail.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Marketplace Kelompok 4',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           // This is the theme of your application.
