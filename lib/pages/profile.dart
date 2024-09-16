b568991fc933015cc131fbd2fc2e58d65df32d3e
Brand0n25 <emmanuelbrandon328@gmail.com>
2024-09-16 13:04:14 +0700

    profilePage

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marketplace_kelompok4/pages/profile_change.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -8,9 +9,245 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final String name = 'John Doe';
+  final String email = 'john.doe@example.com';
+  final String phone = '123-456-7890';
+  final String address = '123 Main Street, City';
+  final String birthday = '1990-01-01';
+  final String _gender = 'Male';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(237, 163, 185, 215),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Edit Account Profile',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+      ),
+      body: Stack(children: [
+        Container(
+          padding: const EdgeInsets.only(top: 60, left: 25, right: 25),
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(237, 163, 185, 215),
+          ),
+        ),
+        Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  height: MediaQuery.of(context).size.height * 0.558,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  decoration: const ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(29),
+                      ),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                           image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0KehqXBrMLd32HsfjDoaq098WeNA0b3g_2A&s")),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0x3F000000),
+                              blurRadius: 4,
+                              offset: Offset(0, 4),
+                              spreadRadius: 0,
+                            )
+                          ],
+                        ),
+                   
+                      ),
+                      _buildInfoField(label: 'Name', value: name),
+                      _buildInfoField(label: 'Email', value: email),
+                      _buildInfoField(label: 'Phone', value: phone),
+                      _buildInfoField(label: 'Address', value: address),
+                      _buildInfoField(label: 'Birthday', value: birthday),
+                      _buildInfoField(label: 'Gender', value: _gender),
+                      const SizedBox(height: 10),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ProfileChangee()),
+                        );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                              top: 15, bottom: 15, left: 95, right: 95),
+                          decoration: ShapeDecoration(
+                            color: const Color(0xFFfd6f3e),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            shadows: const [
+                              BoxShadow(
+                                color: Color(0x3F000000),
+                                blurRadius: 4,
+                                offset: Offset(0, 4),
+                                spreadRadius: 0,
+                              )
+                            ],
+                          ),
+                          child: const Text(
+                            'Edit',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFFFEFFFF),
+                              fontSize: 14,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                InkWell(
+                  onTap: () {
+                    // Tambahkan logika penyimpanan data di sini
+                  },
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: ShapeDecoration(
+                      color: const Color.fromARGB(255, 62, 75, 253),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      shadows: const [
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 4,
+                          offset: Offset(0, 4),
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: Center(
+                      child: const Text(
+                        'Logout',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFFFEFFFF),
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                InkWell(
+                  onTap: () {
+                    // Tambahkan logika penyimpanan data di sini
+                  },
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFFF24340),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      shadows: const [
+                        BoxShadow(
+                          color: Color(0x3F000000),
+                          blurRadius: 4,
+                          offset: Offset(0, 4),
+                          spreadRadius: 0,
+                        )
+                      ],
+                    ),
+                    child: Center(
+                      child: const Text(
+                        'Delete Account',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFFFEFFFF),
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+
+  Widget _buildInfoField({
+    required String label,
+    required String value,
+  }) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 48.0, right: 48, top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              label,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              value,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
-
